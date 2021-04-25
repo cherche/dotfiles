89ec652372d762e0c9a99c3160154553354db435
Ryan Nguyen <contactryannguyen@gmail.com>
2021-04-25 19:53:36 -0400

    Added script to update package lists

---
+++ packages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Back up current Brewfile
+mv Brewfile Brewfile_OLD
+# Generate updated Brewfile
+{
+  brew update && brew upgrade
+  brew bundle dump
+  mv Brewfile_OLD ~/.Trash
+} || {
+  # And if things go horribly wrong, revert
+  mv Brewfile_OLD Brewfile
+}
+
+# Back up current Atomfile
+mv Atomfile Atomfile_OLD
+# Generate updated Atomfile
+{
+  apm upgrade
+  apm list --installed --bare > Atomfile
+  mv Atomfile_OLD ~/.Trash
+} || {
+  mv Atomfile_OLD Atomfile
+}
