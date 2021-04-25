02220bdf866e20ed202da3b53b22956dcd442836
Ryan Nguyen <contactryannguyen@gmail.com>
2021-04-25 19:32:21 -0400

    Created full installation script (system from scratch)

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Link our general dotfiles
+bash './link.sh'
+
+# Install Homebrew if not yet installed
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Homebrew packages from Brewfile
+brew bundle
+
+# Install Atom packages from Atomfile
+apm install --packages-file Atomfile
+
+# Link supplementary dotfiles associated with Mac apps
+bash './apps.sh'
