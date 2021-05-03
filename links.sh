02704279d19ce09485134d2e7faf9d6f77175e71
Ryan Nguyen <contactryannguyen@gmail.com>
2021-05-03 15:25:53 -0400

    Created .tmux.conf

--- links.sh
+++ links.sh
@@ -4,5 +4,6 @@ ln -sv ~/dotfiles/system/.alias ~
 ln -sv ~/dotfiles/system/.env ~
 ln -sv ~/dotfiles/runcom/.zshrc ~
 ln -sv ~/dotfiles/runcom/.inputrc ~
+ln -sv ~/dotfiles/runcom/.tmux.conf ~
 ln -sv ~/dotfiles/git/.gitconfig ~
 ln -sv ~/dotfiles/git/.gitignore ~
