355155617717c4907cf96887595c0c3b01bddb81
Ryan Nguyen <contactryannguyen@gmail.com>
2021-08-21 23:32:15 -0400

    Configure yabai

--- apps.sh
+++ apps.sh
@@ -5,3 +5,4 @@ ln -sv ~/dotfiles/apps/spicetify-config.ini ~/spicetify_data/config-xpui.ini
 ln -sv ~/dotfiles/apps/zathurarc ~/.config/zathura/
 ln -sv ~/dotfiles/apps/alacritty.yml ~/.config/alacritty/
 ln -sv ~/dotfiles/apps/skhdrc ~/.config/skhd/
+ln -sv ~/dotfiles/apps/yabairc ~/.config/yabai/
