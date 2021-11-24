9313c872bcfe6d11fef164f4a40f5408955f4a0b
Ryan Nguyen <contactryannguyen@gmail.com>
2021-11-24 00:31:47 -0500

    spicetify: Update
    Change config path to newly supported .config location

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ln -sv ~/dotfiles/.hyper.js ~
-ln -sv ~/dotfiles/spicetify-config.ini ~/spicetify_data/config-xpui.ini
+ln -sv ~/dotfiles/spicetify-config.ini ~/.config/spicetify/config-xpui.ini
 ln -sv ~/dotfiles/zathurarc ~/.config/zathura/
 ln -sv ~/dotfiles/alacritty.yml ~/.config/alacritty/
 ln -sv ~/dotfiles/skhdrc ~/.config/skhd/
