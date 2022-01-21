2fa9713c46be686e9c94540cc38827a71c5b7ec8
Ryan Nguyen <contactryannguyen@gmail.com>
2022-01-21 11:49:22 -0500

    scripts: Modify autoupdate script to only commit package lists

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -3,6 +3,6 @@
 PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin"
 
 ./scripts/packages.sh
-git add .
-git commit -m "[Auto] Update dotfiles"
+git add Brewfile Atomfile npmfile
+git commit -m "[Auto] Update package lists"
 git push
