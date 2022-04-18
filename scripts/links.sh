87f9c076971b2f0bc4cd680d92b2f1c3a708f6b5
Ryan Nguyen <contactryannguyen@gmail.com>
2022-04-18 19:18:58 -0400

    Reorganized dotfiles

--- scripts/links.sh
+++ scripts/links.sh
@@ -2,20 +2,21 @@
 
 ln -svf ~/dotfiles/shell/alias.sh ~/.config/zsh/
 ln -svf ~/dotfiles/shell/env.sh ~/.config/zsh/
-ln -svf ~/dotfiles/runcom/.bashrc ~/.bashrc
-ln -svf ~/dotfiles/runcom/.zshrc_ohmyzsh ~/.zshrc
-ln -svf ~/dotfiles/runcom/.inputrc ~
-ln -svf ~/dotfiles/.tmux.conf ~
+ln -svf ~/dotfiles/shell/.bashrc ~/.bashrc
+ln -svf ~/dotfiles/shell/.zshrc_ohmyzsh ~/.zshrc
+ln -svf ~/dotfiles/shell/.inputrc ~
+ln -svf ~/dotfiles/tmux/.tmux.conf ~
+
 ln -svf ~/dotfiles/git/.gitconfig ~
 ln -svf ~/dotfiles/git/.gitignore ~
 ln -svf ~/dotfiles/.crontab ~
 
-ln -svf ~/dotfiles/.hyper.js ~
-ln -svf ~/dotfiles/spicetify-config.ini ~/.config/spicetify/config-xpui.ini
-ln -svf ~/dotfiles/zathurarc ~/.config/zathura/
-ln -svf ~/dotfiles/alacritty.yml ~/.config/alacritty/
-ln -svf ~/dotfiles/inkscape-preferences.xml "$HOME/Library/Application Support/org.inkscape.Inkscape/config/inkscape/preferences.xml"
-ln -svf ~/dotfiles/init.sage ~/.sage/
-ln -svf ~/dotfiles/skhdrc ~/.config/skhd/
-ln -svf ~/dotfiles/yabairc ~/.config/yabai/
-ln -svf ~/dotfiles/vimrc ~/.vim/
+ln -svf ~/dotfiles/hyper/.hyper.js ~
+ln -svf ~/dotfiles/spicetify/spicetify-config.ini ~/.config/spicetify/config-xpui.ini
+ln -svf ~/dotfiles/zathura/zathurarc ~/.config/zathura/
+ln -svf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/
+ln -svf ~/dotfiles/inkscape/inkscape-preferences.xml "$HOME/Library/Application Support/org.inkscape.Inkscape/config/inkscape/preferences.xml"
+ln -svf ~/dotfiles/sage/init.sage ~/.sage/
+ln -svf ~/dotfiles/skhd/skhdrc ~/.config/skhd/
+ln -svf ~/dotfiles/yabai/yabairc ~/.config/yabai/
+ln -svf ~/dotfiles/vim/vimrc ~/.vim/
