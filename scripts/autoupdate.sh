cc6ff439f97e56a3c40621e8868729181e36d822
Ryan Nguyen <contactryannguyen@gmail.com>
2022-04-30 08:09:08 -0400

    scripts/audoupdate: Make commit message less obtrusive. Automatically commit more dotfiles

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -3,6 +3,6 @@
 PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin"
 
 ./scripts/packages.sh
-git add Brewfile Atomfile npmfile
-git commit -m "[Auto] Update package lists"
+git add Brewfile Atomfile npmfile inkscape spicetify
+git commit -m ".."
 git push
