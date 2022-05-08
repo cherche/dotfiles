c3384afa821079e17cc9b81946cbe4fd443dce9b
Ryan Nguyen <contactryannguyen@gmail.com>
2022-05-08 12:23:35 -0400

    skhd/macros: Further shorten visual feedback duration to 40ms

--- skhd/macros/flash-active-border.sh
+++ skhd/macros/flash-active-border.sh
@@ -1,5 +1,5 @@
 yabai -m config active_window_border_color "0xff$1"
-sleep .07
+sleep .04
 # We can be sure this is the appropriate border colour
 # since errors can only occur in normal mode
 yabai -m config active_window_border_color '0xfff6c177'
