7eddc9cc70a79169b5e78c3389fab4f8b12f57eb
Ryan Nguyen <contactryannguyen@gmail.com>
2022-05-22 19:01:49 -0400

    mpv: Install theme github:maoiscat/mpv-light-box

--- scripts/links.sh
+++ scripts/links.sh
@@ -16,10 +16,12 @@ ln -svf ~/dotfiles/spicetify/config-xpui.ini ~/.config/spicetify/
 ln -svf ~/dotfiles/zathura/zathurarc ~/.config/zathura/
 ln -svf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/
 ln -svf ~/dotfiles/inkscape/inkscape-preferences.xml "$HOME/Library/Application Support/org.inkscape.Inkscape/config/inkscape/preferences.xml"
-ln -svf ~/dotfiles/mpv/mpv.conf ~/.config/mpv/
 ln -svf ~/dotfiles/sage/init.sage ~/.sage/
 ln -svf ~/dotfiles/skhd/skhdrc ~/.config/skhd/
 ln -svf ~/dotfiles/yabai/yabairc ~/.config/yabai/
+
+ln -svf ~/dotfiles/mpv/mpv.conf ~/.config/mpv/
+ln -svf ~/dotfiles/mpv/scripts/ ~/.config/mpv/
 
 ln -svf ~/dotfiles/vim/vimrc ~/.vim/
 ln -svf ~/dotfiles/vim/after/ ~/.vim/
