9194f25e2ca63b1d7d6abd3707ce7e38db47cd72
Ryan Nguyen <contactryannguyen@gmail.com>
2022-07-29 18:20:47 -0400

    shell/env.sh: Split /Users/ryan/dotfiles/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/opt/X11/bin:/opt/homebrew/opt/fzf/bin across multiple lines for better readability

--- shell/env.sh
+++ shell/env.sh
@@ -1,4 +1,15 @@
-export PATH=$HOME/dotfiles/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:$HOME/.cargo/bin:/opt/X11/bin
+export PATH="\
+$HOME/dotfiles/bin\
+:/opt/homebrew/bin\
+:/opt/homebrew/sbin\
+:/usr/local/bin\
+:/usr/bin\
+:/bin\
+:/usr/sbin\
+:/sbin\
+:/Library/TeX/texbin\
+:/opt/X11/bin\
+"
 export LANG=en_CA.UTF-8
 export EDITOR=vim
 
