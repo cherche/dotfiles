d68c870c512a7b8d685b6c4ee8a7543d971102b1
Ryan Nguyen <contactryannguyen@gmail.com>
2022-08-28 20:37:10 -0400

    n8n: Execute directly from cron instead of n8n session

--- launchd/n8n.sh
+++ launchd/n8n.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 # Remember to load org.n8n.ta before screaming in frustration!
+# > cp ~/dotfiles/launchd/org.n8n.ta.plist ~/Library/LaunchAgents/
+# > launchctl load ~/Library/LaunchAgents/org.n8n.ta.plist
 tmux new-session -d -s n8n 'n8n start'
---
+++ scripts/n8n.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This gives us `n8n` and `node` and more goodies
+PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin"
+
+n8n execute --id 3
