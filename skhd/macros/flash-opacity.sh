9fb6e4f0cca1301f977245ce05a4b5fb532f8251
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2023-10-14 22:30:33 -0400

    skhd: Improved opacity flashes

--- skhd/macros/flash-opacity.sh
+++ skhd/macros/flash-opacity.sh
@@ -1,9 +1,11 @@
 # $1 - opacity
 # $2 - number of repetitions
+# $3 - length
+len=${3:-0.07}
 for (( c=1; c<=$2; c++ ))
 do 
     yabai -m config active_window_opacity $1
-    sleep .05
+    sleep $len
     yabai -m config active_window_opacity 1.0
     sleep .03
 done
